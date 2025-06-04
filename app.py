3f555a6b81e013f0896c280612d92c1436d1cd55
Rebugha <renughaveerappan@gmail.com>
2025-06-04 16:49:44 +0530

    popup after entry

--- app.py
+++ app.py
@@ -5,26 +5,21 @@ import random
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('web.html', message='welcome to the registration page!')
+def home():
+    success = request.args.get('success')
+    return render_template('web.html', success=success)
 
 @app.route('/register', methods=['POST'])
 def register():
     name = request.form['name']
     contact = request.form['contact']
-    #phone = request.form.get('phone', '').strip()
-    #name = request.form.get('name', '').strip()
 
-    #formatted_phone = ''.join(filter(str.isdigit, contact))
-    #formatted_name = ''.join(filter(str.isalpha, name))
-
-    # Save the number to a file
     with open('contacts.csv', 'a', newline='') as file :
         writer = csv.writer(file)
         writer.writerow([name, contact])
 
 
-    return redirect('/')
+    return redirect(url_for('home', success='true'))
 
 
 #to make images slide 
@@ -32,7 +27,7 @@ def register():
 images = ['Frame1.webp', 'Frame2.webp', 'frame3.webp']
 
 @app.route('/')
-def home():
+def image():
     selected_image = random.choice(images)
     image_url = url_for('static', filename=selected_image)
     return render_template('web.html', image_url=image_url)
